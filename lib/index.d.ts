5599cbc5c7e259dd5e8cd43b92e482a9d373d34f
Connor Love <connor@dotconnor.com>
2019-02-11 12:31:47 -0500

    remove lib from git

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-export default function defer(): DeferedPromise;
-declare type PromiseFunction = (value?: {} | PromiseLike<{}> | undefined) => void;
-export declare class DeferedPromise extends Promise<any> {
-    resolve: PromiseFunction;
-    reject: PromiseFunction;
-}
-export {};
