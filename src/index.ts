f86bca721f4e1bdf7c4947feb25dea0d531f0564
Connor Love <connor@dotconnor.com>
2019-06-03 20:01:26 -0400

    ref: update code style

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 export default function defer(): DeferedPromise {
-	let res: PromiseFunction = () => void 0;
-	let rej: PromiseFunction = () => void 0;
+  let res: PromiseFunction = () => void 0;
+  let rej: PromiseFunction = () => void 0;
 
-	var promise: DeferedPromise = new DeferedPromise((resolve, reject) => {
-		res = resolve;
-		rej = reject;
-	});
+  const promise: DeferedPromise = new DeferedPromise((resolve, reject) => {
+    res = resolve;
+    rej = reject;
+  });
 
-	promise.resolve = res;
-	promise.reject = rej;
+  promise.resolve = res;
+  promise.reject = rej;
 
-	return promise;
+  return promise;
 }
 type PromiseFunction = (value?: {} | PromiseLike<{}> | undefined) => void;
 export class DeferedPromise extends Promise<any> {
-	public resolve: PromiseFunction = () => void 0;
-	public reject: PromiseFunction = () => void 0;
+  public resolve: PromiseFunction = () => void 0;
+  public reject: PromiseFunction = () => void 0;
 }
